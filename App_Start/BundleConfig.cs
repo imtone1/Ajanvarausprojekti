20795ed4384bce5a6773fbbff66003f51e579b92
Jon Haataja <jon.haataja@student.careeria.fi>
2022-05-04 11:32:58 +0300

    Lisäsin koko-optimoituja kuvia ja aloitin index.cshtml muotoilut

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -26,7 +26,7 @@ namespace Ajanvarausprojekti
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
 
     
 
