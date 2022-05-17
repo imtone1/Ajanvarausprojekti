cd2b1c1f8835d331f8d3bf1c7494bac5d4a7d670
Dora Hytönen <dora.hytonen@student.careeria.fi>
2022-05-17 18:07:45 +0300

    LueViisi renderöinti toimii Opettaja idn perusteella valikoituvat palautteet toimii lue lisää toiminto ei toimi

---
+++ ViewModels/palauteListaData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace Ajanvarausprojekti.ViewModels
+{
+    public class palauteListaData
+    {
+        public int opettaja_id { get; set; }
+        [Required(ErrorMessage = "Etunimi on annettava")]
+        public string etunimi { get; set; }
+        [Required(ErrorMessage = "Sukunimi on annettava")]
+        public string sukunimi { get; set; }
+        [Required(ErrorMessage = "Nimike on annettava")]
+        public int palaute_id { get; set; }
+        public string palautetyyppi { get; set; }
+        public string palaute { get; set; }
+        public System.DateTime palaute_pvm { get; set; }
+        public Nullable<int> palautetyyppi_id { get; set; }
+
+    }
+}
