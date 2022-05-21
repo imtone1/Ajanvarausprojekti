8129760221abadc54f9d458fe29abb153ad02cd8
Susanna Kalijarvi <susanna.kalijarvi@student.careeria.fi>
2022-05-21 13:00:46 +0300

    Lisätty AjatControlleriin tietokantayhteys ja loppuun disbose

--- Controllers/AjatController.cs
+++ Controllers/AjatController.cs
@@ -3,15 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ajanvarausprojekti.Models;
 
 namespace Ajanvarausprojekti.Controllers
 {
     public class AjatController : Controller
     {
+        //Luodaan tietokantayhteys ylätasolla jolloin se näkyy jokaiseen metodiin
+        aikapalauteEntities db = new aikapalauteEntities();
+
         // GET: Ajat
         public ActionResult Index()
         {
             return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //vapautetaan lopussa tietokantayhteys Disposella
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
