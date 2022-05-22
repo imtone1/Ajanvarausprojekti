01db64ae3c49ddb99d5aa7a17eb9849b9b19974d
Heli Kulomaa <88309715+helikulomaa@users.noreply.github.com>
2022-05-22 09:50:55 +0300

    Opekortit merge (#54)
    * Välitallennus
    
    * Opettajajien korttinäkymä valmis! Vaihdettu opekuvat-kansion kuvat. Koodi lisätty viimeisimpään Sprint2-haaran koodiin.
    
    * Lisätty vielä default-kuva
    
    * Entity Framework päivitetty.

---
+++ Controllers/OpeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ajanvarausprojekti.Models;
+
+namespace Ajanvarausprojekti.Controllers
+{
+    public class OpeController : Controller
+    {
+
+        //Luodaan tietokantayhteys ylätasolla jolloin se näkyy jokaiseen metodiin
+        aikapalauteEntities db = new aikapalauteEntities();
+        //vapautetaan olio lopussa omalla metodillaan. Tällöin sitä ei tarvitse vapauttaa jokaisessa metodissa erikseen.
+
+        // GET: Ope
+        public ActionResult Index()
+        {
+            List<Opettajat> model = db.Opettajat.ToList();
+            return View(model);
+        }
+        
+
+        public ActionResult Opekortit()
+        {
+            List<Opettajat> model = db.Opettajat.ToList();
+            return PartialView("Opekortit", model);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
