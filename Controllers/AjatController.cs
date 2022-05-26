2770d073da44c4b814e469e27fdc3976f8f2311d
Susanna Kalijarvi <susanna.kalijarvi@student.careeria.fi>
2022-05-26 16:51:09 +0300

    lisätty näkymä _LisaaAika

--- Controllers/AjatController.cs
+++ Controllers/AjatController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using Ajanvarausprojekti.Models;
 using Ajanvarausprojekti.ViewModels;
