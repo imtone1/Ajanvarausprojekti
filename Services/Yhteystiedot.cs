af2f7b0b91239aaac6ce1797f0137c114dc46f56
Irina <88165529+imtone1@users.noreply.github.com>
2022-06-02 18:26:40 +0300

    sähköpostilähetystoiminto korjattu (rikki mennyt gmail salasanakäytäntöjen koventamisen takia: "Tilin suojaamiseksi Google ei enää 30.5.2022 alkaen tue sellaisten kolmansien osapuolten sovelluksia tai laitteita, jotka pyytävät sinua kirjautumaan Google-tilille vain käyttäjätunnuksella ja salasanalla.") (#77)
    Mergaan tämän nyt näin heti, koska koodissa ei muutettu mitään muutä kuin sähköpostin salasanan

--- Services/Yhteystiedot.cs
+++ Services/Yhteystiedot.cs
@@ -9,7 +9,7 @@ namespace Ajanvarausprojekti.Services
     {
         //Nämä gmail osoitteita, koska server gmail
         public string OhjelmanSahkopostiosoite = "tivisovellus@gmail.com";
-        public string OhjelmanSpostiSalasana = "1hAn5!VAiO1k9";
+        public string OhjelmanSpostiSalasana = "kxgkmvavjrqoeubz";
 
         //default opekuva, tämä on vain polku eli tämän kuvan pitäisi olla lisättynä Opekuva -tiedostoon ensin
         public string OpeDefaultKuva = "/Opekuvat/defaultKuva.jpg";
