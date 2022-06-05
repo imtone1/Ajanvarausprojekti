8806aed2b444291a1a6778af86a38e8ea0107342
Heli Kulomaa <heli.kulomaa@gmail.com>
2022-06-05 09:21:52 +0300

    Opettajan vapaat ajat listautuvat modaali-ikkunaan. Aika-id vielä kiinnittämättä varaukseen.

--- Controllers/OpeController.cs
+++ Controllers/OpeController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Ajanvarausprojekti.Models;
+using Ajanvarausprojekti.ViewModels;
 
 namespace Ajanvarausprojekti.Controllers
 {
@@ -28,7 +29,59 @@ namespace Ajanvarausprojekti.Controllers
             List<Opettajat> model = db.Opettajat.ToList();
             return PartialView("Opekortit", model);
         }
+        public ActionResult _VapaatAjat(int? opeid)
+        {
+            // LIstataan kaikki kyseisen opettajan ajat
 
+            var ajatLista = (from a in db.Ajat
+                             join o in db.Opettajat on a.opettaja_id equals o.opettaja_id
+                             join k in db.Kestot on a.kesto_id equals k.kesto_id
+                             where o.opettaja_id == opeid
+                             where a.alku_aika >= DateTime.Today
+                             orderby a.alku_aika
+
+                             select new ajatListaData
+                             {
+                                 Etunimi = o.etunimi,
+                                 Sukunimi = o.sukunimi,
+                                 aika_id = (int)a.aika_id,
+                                 Alkuaika = (DateTime)a.alku_aika,
+                                 Kesto = (int)k.kesto,
+                                 opettaja_id = (int)a.opettaja_id,
+                                 Paikka = a.paikka
+                             }).ToList();
+
+            // Listataan kyseisen opettajan varatut ajat
+
+            var varatut = (from a in db.Ajat
+                           join o in db.Opettajat on a.opettaja_id equals o.opettaja_id
+                           join k in db.Kestot on a.kesto_id equals k.kesto_id
+                           join v in db.Varaukset on a.aika_id equals v.aika_id
+                           where o.opettaja_id == opeid
+                           where a.alku_aika >= DateTime.Today
+                           where a.aika_id == v.aika_id
+                           orderby a.alku_aika
+
+
+                           select new ajatListaData
+                           {
+                               Etunimi = o.etunimi,
+                               Sukunimi = o.sukunimi,
+                               aika_id = (int)a.aika_id,
+                               Alkuaika = (DateTime)a.alku_aika,
+                               Kesto = (int)k.kesto,
+                               opettaja_id = (int)a.opettaja_id,
+                               Paikka = a.paikka
+                           }).ToList();
+
+            // Listataan vapaat ajat poistamalla kaikista ajoista varatut ajat
+
+            var vapaatAjat = (from a in ajatLista
+                              where !varatut.Any(x => x.aika_id == a.aika_id)
+                              select a).ToList();
+
+            return PartialView("_VapaatAjat", vapaatAjat);
+        }
 
         protected override void Dispose(bool disposing)
         {
