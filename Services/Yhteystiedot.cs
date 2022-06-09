e9b067d9cd9f6e47a01c8a8baba3fea5f5b69de2
Irina <88165529+imtone1@users.noreply.github.com>
2022-06-09 22:19:07 +0300

    Yhteystiedot Yhteystiedo classista määrättäväksi, Viewt otsikoiden su… (#95)
    * Yhteystiedot Yhteystiedo classista määrättäväksi, Viewt otsikoiden suomenkielisiksi, tietosuojaselosteen viilaus
    
    * Käyttöehdot checkbox ja korjaukset
    
    * korjaus

--- Services/Yhteystiedot.cs
+++ Services/Yhteystiedot.cs
@@ -11,6 +11,22 @@ namespace Ajanvarausprojekti.Services
         public string OhjelmanSahkopostiosoite = "tivisovellus@gmail.com";
         public string OhjelmanSpostiSalasana = "kxgkmvavjrqoeubz";
 
+
+        //sivuston nimi
+        public string SivustonNimi = "Careerian Tivi-ohjaus";
+        //tietosuoja, käyttäjätiedot, yhteystiedot
+        public string Rekisterinpitaja = "Simo Siren, simo.siren@careeria.fi, TÄHÄN PUH";
+        public string Rekisterinpitäjan_yhteyshenkilo = "Opettaja Simo Siren";
+        public string Henkilorekisterin_vastuuhenkilo = "Opettaja Simo Siren";
+        public string Tietosuojavastaava = "Simo Siren, simo.siren @careeria.fi";
+
+        //Careerian Tivi-ohjaus-palvelun ylläpidosta vastaa 
+        public string YllapitoyhtTiedot="tivi-opettaja Simo Siren";
+        //mailto:
+        public string EmailYhteystiedot = "Simo.Siren@careeria.fi";
+        //puh.nro Yhteystiedot sivulla
+        public string Puhnro = "";
+
         //määrittelee millä intervallilla poistetaan vanhentuneita varauksia, nyt on 30pv. 
         public int PoistoIntervalli = -30;
 
